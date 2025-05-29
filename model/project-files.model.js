789e6c4c1ea5286f88a404e8ca0743de7cec3c27
kailas <kailas.nath@icodebees.com>
2025-05-29 10:59:44 +0530

    path added to project

--- model/project-files.model.js
+++ model/project-files.model.js
@@ -1,4 +1,5 @@
-const { DataTypes } = require('sequelize');
+// model/project-files.model.js
+const { DataTypes } = require('sequelize'); 
 const sequelize = require('../config/sequelize');
 
 const ProjectFile = sequelize.define('ProjectFile', {
@@ -10,6 +11,7 @@ const ProjectFile = sequelize.define('ProjectFile', {
   projectId: {
     type: DataTypes.INTEGER,
     allowNull: false,
+    field: 'project_id', // Maps to database column project_id
     references: {
       model: 'projects',
       key: 'id'
@@ -17,37 +19,45 @@ const ProjectFile = sequelize.define('ProjectFile', {
   },
   filename: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    field: 'filename' // Database column is already correct
   },
   fileType: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    field: 'file_type' // Maps to database column file_type
   },
   fileSize: {
     type: DataTypes.INTEGER,
-    allowNull: false
+    allowNull: false,
+    field: 'file_size' // Maps to database column file_size
   },
   filePath: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    field: 'file_path' // Maps to database column file_path
   },
   mimetype: {
     type: DataTypes.STRING,
-    allowNull: false
+    allowNull: false,
+    field: 'mimetype' // Database column is already correct
   },
   isZipExtracted: {
     type: DataTypes.BOOLEAN,
-    defaultValue: false
+    defaultValue: false,
+    field: 'is_zip_extracted' // Maps to database column is_zip_extracted
   },
   originalZipName: {
     type: DataTypes.STRING,
-    allowNull: true
+    allowNull: true,
+    field: 'original_zip_name' // Maps to database column original_zip_name
   }
 }, {
   tableName: 'project_files',
-  timestamps: true
+  timestamps: true,
+  underscored: true, // This tells Sequelize to expect snake_case in DB
+  createdAt: 'created_at',
+  updatedAt: 'updated_at'
 });
-
-
 
 module.exports = ProjectFile;
